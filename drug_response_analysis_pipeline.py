3c74ee440bdf5234e9bc15d9387d137da8d2e1a9
Janice Patterson <patterja@ohsu.edu>
2020-04-15 22:02:37 -0700

    Chaned Bunch of SettingWithCopyWarning

--- drug_response_analysis_pipeline.py
+++ drug_response_analysis_pipeline.py
@@ -2,8 +2,9 @@
 
 #program: drug_response_analysis_pipeline.py
 #Author: Hongmei Zhang
-#email:zhanho@ohsu.edu
-#data:Fri Feb 24 13:47:05 2017
+#data:Fri Feb 24 13:47:05 2017 v1
+#Edited: Janice Patterson
+#v2 edits 2020-04-15
 
 #description
 #Glue code for pipeline
@@ -11,7 +12,7 @@
 #import libraries
 import sys
 import os
-import pandas as pd
+import shutil
 import re
 import csv
 import argparse
@@ -35,7 +36,6 @@ parser.add_argument('--sc', help='input the drug_serum_conc.txt')
 args = parser.parse_args()
 
 ##load path of scripts
-#sys.path.append("/Users/zhanho/Project/SMMART/scripts/")
 #from drug_plate_median import CellCountMedian
 #from generate_input_for_gr50_tools import time0_median, generate_input
 
@@ -117,35 +117,59 @@ with open (args.s, "r") as tsvsamp:
 s_gr50_input=os.path.join(dir_CellCount, "input_file_of_gr50_tools.txt")
 with open (s_gr50_input, "w") as tsvout:
     h_out=csv.writer(tsvout, delimiter="\t")
-    lst_header=["cell_line", "agent", "time", "concentration", "cell_count", "cell_count__ctrl", "cell_count__time0"]
+    lst_header=["cell_line", "agent", "timepoint", "concentration", "cell_count", "cell_count__ctrl", "cell_count__time0"]
     h_out.writerow(lst_header)
     for key in d_output.keys():
         h_out.writerow(d_output[key])
 
 ###########calculate the GR values and metrics###########
+print("running gr50 tools")
 s_GRvalue=os.path.join(dir_GR, "GRvalues.txt")
 s_GRmetrics=os.path.join(dir_GR, "GRmetrics.txt")
-#os.system("/usr/bin/python /Users/patterja/Workspace/gr50_tools/SRC/python/scripts/add_gr_column.py %s > %s" % (s_gr50_input, s_GRvalue))
-#os.system("/usr/bin/python /Users/patterja/Workspace/gr50_tools/SRC/python/scripts/compute_gr_metrics.py %s > %s" % (s_GRvalue, s_GRmetrics))
-#os.system("python3 /Users/patterja/Workspace/hongmei/scripts/plot_figure.py --v %s --m %s --d %s --d2 %s" % (s_GRvalue, s_GRmetrics, dir_drug_plot, dir_cell_line_plot))
+
+# Run below code separately after installing gr50_tools from hafner and using his tool which is built with python2
+os.system("/usr/bin/python /Users/patterja/Workspace/gr50_tools_repos/gr_metrics/SRC/python/scripts/add_gr_column.py %s > %s" % (s_gr50_input, s_GRvalue))
+os.system("/usr/bin/python /Users/patterja/Workspace/gr50_tools_repos/gr_metrics/SRC/python/scripts/compute_gr_metrics.py %s > %s" % (s_GRvalue, s_GRmetrics))
+os.system("/usr/bin/python /Users/patterja/Workspace/hongmei/drug_response/plot_curves.py --metrics %s --values %s --outdir_bydrug %s --outdir_bycellline %s" % (s_GRmetrics, s_GRvalue, dir_drug_plot, dir_cell_line_plot))
 #os.system("python3 /Users/patterja/Workspace/hongmei/scripts/plot_figure.py --v %s --m %s --d %s" % (s_GRvalue, s_GRmetrics, dir_drug_plot))
 
 ############add max does of each drug into the GRmetrics.txt############
-#df_GRm_edit=add_max_conc_to_GRm(f_GRm=s_GRmetrics, f_layout=args.l)
-#df_GRm_edit.to_csv(s_GRmetrics, sep="\t")
+# make copy of original GRmetrics file to prevent confusion
+shutil.copy(s_GRmetrics, os.path.join(dir_GR, "GRmetrics_original.txt"))
+df_GRm_edit=add_max_conc_to_GRm(f_GRm=s_GRmetrics, f_layout=args.l)
+df_GRm_edit.to_csv(s_GRmetrics, sep="\t")
 
 ###########GR matrics statistics plot####################
-os.system('python3 /Users/patterja/Workspace/hongmei/drug_response/boxplot_for_GRmetrics.py --i %s --w %s' % (s_GRmetrics, dir_stat_plot))
-os.system('python3 /Users/patterja/Workspace/hongmei/drug_response/gr50_and_curve_count_barplot.py --i %s --p %s --w %s' % (s_GRmetrics, "0.05", dir_stat_plot))
 
+os.system('python /Users/patterja/Workspace/hongmei/drug_response/boxplot_for_GRmetrics.py --i %s --w %s' % (s_GRmetrics, dir_stat_plot))
+os.system('python /Users/patterja/Workspace/hongmei/drug_response/gr50_and_curve_count_barplot.py --i %s --p %s --w %s' % (s_GRmetrics, "0.05", dir_stat_plot))
+print("\nFinished GR matrics statistics plots\n")
 ###########GR50 and drug serum concentration plot#############
-os.system('python3 /Users/patterja/Workspace/hongmei/drug_response/barplot_for_GR50_and_serum_conc.py --i %s --c %s --s %s --w %s' % (s_GRmetrics, args.c, args.sc, dir_serum_plot))
+
+print("\nStarting GR50 and drug serum concentration plot\n")
+os.system('python /Users/patterja/Workspace/hongmei/drug_response/barplot_for_GR50_and_serum_conc.py --i %s --c %s --s %s --w %s' % (s_GRmetrics, args.c, args.sc, dir_serum_plot))
+print('python /Users/patterja/Workspace/hongmei/drug_response/barplot_for_GR50_and_serum_conc.py --i %s --c %s --s %s --w %s' % (s_GRmetrics, args.c, args.sc, dir_serum_plot))
+
+
 ###########GRmax plot#############
+
+print("\nStarting GRmax plot\n")
+
 #calculate the GR values of drugs at serum concentration, and add the values into GRmetrics
 s_GRmetrics_new=os.path.join(dir_GR, "GRmetrics_GRserum_added.txt")
-os.system('python3 /Users/patterja/Workspace/hongmei/drug_response/calculate_GR_at_serum_conc.py --m %s --gr %s --s %s --o %s' % (s_GRmetrics, s_GRvalue, args.sc, s_GRmetrics_new))
-os.system('python3 /Users/patterja/Workspace/hongmei/drug_response/barplot_for_GRmax_and_GRserum.py --i %s --c %s  --w %s' % (s_GRmetrics_new, args.c, dir_GRmax_plot))
+os.system('python /Users/patterja/Workspace/hongmei/drug_response/calculate_GR_at_serum_conc.py --m %s --gr %s --s %s --o %s' % (s_GRmetrics, s_GRvalue, args.sc, s_GRmetrics_new))
+os.system('python /Users/patterja/Workspace/hongmei/drug_response/barplot_for_GRmax_and_GRserum.py --i %s --c %s  --w %s' % (s_GRmetrics_new, args.c, dir_GRmax_plot))
+print(('python /Users/patterja/Workspace/hongmei/drug_response/barplot_for_GRmax_and_GRserum.py --i %s --c %s  --w %s' % (s_GRmetrics_new, args.c, dir_GRmax_plot)))
 
 ###########GR50 heatmap plot############
-os.system('python3 /Users/patterja/Workspace/hongmei/drug_response/heatmap_for_GR50_median_center.py --i %s --c %s --w %s' % (s_GRmetrics, args.c, dir_stat_plot))
+
+print("\nGR50 heatmap plot\n")
+
+os.system('python /Users/patterja/Workspace/hongmei/drug_response/heatmap_for_GR50_median_center.py --i %s --c %s --w %s' % (s_GRmetrics, args.c, dir_stat_plot))
+os.system(
+    'Rscript /Users/zhanho/Project/SMMART/scripts/heatmap3_new.R --data %s --t %s --r_side_label %s --c_side_label %s --w %d --h %d --min %f --max %f --out %s' % (
+    gr50_file, "'Log10(GR50) nM'", drug_class_file, cc_class_file, 20, 10, -2, 2, heatmap_file))
+
+print('python /Users/patterja/Workspace/hongmei/drug_response/heatmap_for_GR50_median_center.py --i %s --c %s --w %s' % (s_GRmetrics, args.c, dir_stat_plot))
+print("The End")
 
